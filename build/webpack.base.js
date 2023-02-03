238692326228bfc39c6db697489e081d601c2d34
nicole <nicole@chxian.com>
2023-02-03 18:28:05 +0800

    feat: init project and set webpack config

---
+++ build/webpack.base.js
@@ -0,0 +1,41 @@
+// 公共配置
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // 配置入口文件
+  entry: path.join(__dirname, '../src/index.tsx'),
+  // 配置出口文件
+  output: {
+    filename: 'static/js/[name].js', // 每个输出js的名称
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: '/', // 打包后文件的公共路径前缀
+  },
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+            presets: ['@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    // extensions是webpack的resolve解析配置下的选项，
+    // 在引入模块时不带文件后缀时，会来该配置数组里面依次添加后缀查找文件，
+    // 因为ts不支持引入以 .ts, tsx为后缀的文件，所以要在extensions中配置，而第三方库里面很多引入js文件没有带后缀，所以也要配置下js
+    extensions: ['js', 'tsx', 'ts'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
+      inject: true, // 自动注入静态资源
+    }),
+  ],
+};
---
+++ note.md
@@ -0,0 +1,5 @@
+##### 打包后的 dist 文件可以在本地借助 node 服务器 serve 打开,全局安装 serve
+
+`npm i serve -g`
+
+##### 然后在项目根目录命令行执行 serve -s dist,就可以启动打包后的项目了
